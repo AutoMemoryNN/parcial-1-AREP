7f4ef0ed0b6607ebf415e9384f8bf1bb341044b5
lab lab <lab@labinfo.is.escuelaing.edu.co>
2025-02-21 11:37:09 -0500

    feat: basic web server functionality

--- backend/WebServer.java
+++ backend/WebServer.java
@@ -1,5 +1,53 @@
-public class WebServer {
-    public WebServer() {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class WebServer {
+    public WebServer() throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(8080);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 8080.");
+            System.exit(1);
+        }
+
+        Socket clientSocket = null;
+        try {
+            System.out.println("Listo para recibir ...");
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.err.println("Accept failed.");
+            System.exit(1);
+        }
+        PrintWriter out = new PrintWriter(
+                clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        String inputLine, outputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println("Recibí: " + inputLine);
+            if (!in.ready()) {
+                break;
+            }
+        }
+        outputLine = "<!DOCTYPE html>" +
+                "<html>" +
+                "<head>" +
+                "<meta charset=\"UTF-8\">" +
+                "<title>Title of the document</title>\n" +
+                "</head>" +
+                "<body>" +
+                "<h1>Mi propio mensaje</h1>" +
+                "</body>" +
+                "</html>";
+        out.println(outputLine);
+        out.close();
+        in.close();
+        clientSocket.close();
+        serverSocket.close();
     }
 }
